627b9fb0850bbbf3ff7e4384c06f54e75eae343a
DavidIfebueme <davidifebueme@gmail.com>
2024-06-18 21:55:21 +0100

    template changes

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,6 @@ import subprocess
 
 main = Blueprint('main', __name__)
 
-# Configure Web3
-# w3 = Web3(Web3.HTTPProvider(os.getenv('WEB3_PROVIDER')))
-# with open('contracts/VoterVerse.json') as f:
-#     contract_data = json.load(f)
-# contract_address = Web3.toChecksumAddress(os.getenv('CONTRACT_ADDRESS'))
-# contract = w3.eth.contract(address=contract_address, abi=contract_data['abi'])
-
 @main.route('/')
 def index():
     return render_template('index.html')
@@ -24,14 +17,6 @@ def create_election():
     w3.eth.wait_for_transaction_receipt(tx_hash)
     return redirect(url_for('main.index'))
 
-@main.route('/start_voting', methods=['POST'])
-def start_voting():
-    university_id = int(request.form['university_id'])
-    election_id = int(request.form['election_id'])
-    tx_hash = contract.functions.startVoting(university_id, election_id).transact({'from': w3.eth.accounts[0]})
-    w3.eth.wait_for_transaction_receipt(tx_hash)
-    return redirect(url_for('main.index'))
-
 @main.route('/end_election', methods=['POST'])
 def end_election():
     university_id = int(request.form['university_id'])
@@ -40,15 +25,9 @@ def end_election():
     w3.eth.wait_for_transaction_receipt(tx_hash)
     return redirect(url_for('main.index'))
 
-@main.route('/register_voter', methods=['POST'])
+@main.route('/register_voter', methods=['GET'])
 def register_voter():
-    university_id = int(request.form['university_id'])
-    election_id = int(request.form['election_id'])
-    voter_address = request.form['voter_address']
-    nullifier_hash = int(request.form['nullifier_hash'])
-    tx_hash = contract.functions.registerVoter(university_id, election_id, voter_address, nullifier_hash).transact({'from': w3.eth.accounts[0]})
-    w3.eth.wait_for_transaction_receipt(tx_hash)
-    return redirect(url_for('main.index'))
+    return render_template('register.html')
 
 @main.route('/generate_proof', methods=['POST'])
 def generate_proof():
@@ -70,15 +49,9 @@ def generate_proof():
         'public_signals': public_signals
     }
 
-@main.route('/cast_vote', methods=['POST'])
+@main.route('/cast_vote', methods=['GET', 'POST'])
 def cast_vote():
-    university_id = int(request.form['university_id'])
-    election_id = int(request.form['election_id'])
-    nullifier_hash = int(request.form['nullifier_hash'])
-    proof = json.loads(request.form['proof'])
-    tx_hash = contract.functions.castVote(university_id, election_id, nullifier_hash, proof).transact({'from': w3.eth.accounts[0]})
-    w3.eth.wait_for_transaction_receipt(tx_hash)
-    return redirect(url_for('main.index'))
+    return render_template('vote.html')
 
 @main.route('/results', methods=['GET'])
 def results():
